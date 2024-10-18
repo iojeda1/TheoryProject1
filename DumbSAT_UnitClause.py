09003025d35666ed65d9dcd30a28f91e27c7b5f0
Phoebe Huang <chuang26@nd.edu>
2024-10-17 21:22:20 -0400

    DumbSAT_UnitClause.py

--- DumbSAT_UnitClause.py
+++ DumbSAT_UnitClause.py
@@ -1,25 +1,11 @@
 # Unit Clause 
+# Theory of Computing Project01
+# Isabel Ojeda, Phoebe Huang, Pablo Oliva Quintana 
 
 import time
 import random
 import string
-
-
-# personal tests 
-#Wff = [[1, -2], [1, 3], [2], [4]]
-#Nvars = 4
-
-#Wff = [[1], [-1]]
-#Nvars = 1
-
-#Nvars=3
-#Nclauses=5
-#Wff=[[1,-2,-2],[2,3,3],[-1,-3,-3],[-1,-2,3],[1,2,-3]]
-
-
-#Nuclauses=8
-#Wff=[[-1,-2,-3],[-1,-2,3],[-1,2,-3],[-1,2,3],[1,-2,-3],[1,-2,3],[1,2,-3],[1,2,3]]
-#Nvars = 3
+import csv
 
 def unit_clause(Wff,Assignment): 
     c = True
@@ -134,180 +120,43 @@ def test_cases(num):
 
 def test_wff(num):
     wff, Nvars, Nclauses = test_cases(num)
-    Assignment=list((0 for x in range(Nvars+2)))
-    start = time.time() # Start timer
-    SatFlag=check(wff,Nvars,Nclauses,Assignment)
-    end = time.time() # End timer
-    exec_time=int((end-start)*1e6)
-    #return [wff,Assignment,SatFlag,exec_time]
-    #return SatFlag, Assignment, exec_time
-    print(f"Test Case {num}:")
-    print(f"SAT Result: {'Satisfiable' if SatFlag else 'Unsatisfiable'}")
-    print(f"Assignment: {Assignment[1:Nvars+1]}")
+    Assignment = list((0 for x in range(Nvars+2)))
+    start = time.time()  # Start timer
+    SatFlag = check(wff, Nvars, Nclauses, Assignment)
+    end = time.time()  # End timer
+    exec_time = int((end - start) * 1e6)  # Convert to microseconds
+    
+    result = {
+        "Test Case": num,
+        "WFF": wff,
+        "SAT Result": 'Satisfiable' if SatFlag else 'Unsatisfiable',
+        "Assignment": Assignment[1:Nvars+1] if SatFlag else 'N/A',
+        "Execution Time (microseconds)": exec_time
+    }
+    
+    return result
 
 def run_cases():
-    start = time.time() # Start timer
-    for num in range(1, 6): 
-        test_wff(num)
-    end = time.time() # End timer
-    exec_time=int((end-start)*1e6)
-    print(f"Execution Time f: {exec_time}")
+    results = []
+    start = time.time()  # Start timer
+    for num in range(1, 6):
+        result = test_wff(num)
+        results.append(result)  # Append result for each test case
+    end = time.time()  # End timer
+    exec_time = int((end - start) * 1e6)
+    
+    return results, exec_time
 
-run_cases()
+# Run test cases and save results to a CSV file
+results, total_exec_time = run_cases()
 
-'''
-def test_case(Wff, Nvars): 
-    Nclauses = len(Wff)
-    Assignment = [0] * (Nvars + 2)
-    result = check(Wff, Nvars, Nclauses, Assignment)
+# Now write results to CSV
+with open('unit.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Test Case", "WFF", "SAT Result", "Assignment", "Execution Time (microseconds)"])  # Write header
+    for result in results:
+        writer.writerow([result["Test Case"], result["WFF"], result["SAT Result"], result["Assignment"], result["Execution Time (microseconds)"]])
 
-    if result:
-        print('Satisfiable')
-        print("Assignment", Assignment[1:len(Assignment)-1])
+# Optional: print total execution time
+print("Results succesfully written into unit.csv")
 
-    else:
-        print("Unsatisfiable")
-
-test_case(Wff, Nvars)
-
-def run_cases(TestCases,ProbNum,resultsfile,tracefile,cnffile):
-    # TestCases: list of 4tuples describing problem
-    #   0: Nvars = number of variables
-    #   1: NClauses = number of clauses
-    #   2: LitsPerClause = Literals per clause
-    #   3: Ntrials = number of trials
-    # ProbNum: Starting nunber to be given to 1st output run
-    # resultsfile: path to file to hold output
-    # tracefile: path to file to hold output
-    # cnffile: path to file to hold output
-    # For each randomly built wff, print out the following list
-    #   Problem Number
-    #   Number of variables
-    #   Number of clauses
-    #   Literals per clause
-    #   Result: S or U for satisfiable or unsatisfiable
-    #   A "1"
-    #   Execution time
-    #   If satisfiable, a binary string of assignments
-    if not(ShowAnswer):
-        print("S/U will NOT be shown on cnf file")
-# Each case = Nvars,NClauses,LitsPerClause,Ntrials
-    f1=open(resultsfile+".csv",'w')
-    f2=open(tracefile+".csv",'w')
-    f3=open(cnffile+".cnf","w")
-    #initialize counters for final line of output
-    Nwffs=0
-    Nsat=0
-    Nunsat=0
-#    f1.write('ProbNum,Nvars,NClauses,LitsPerClause,Result,ExecTime(us)\n')
-    for i in range(0,len(TestCases)):
-        TestCase=TestCases[i]
-        Nvars=TestCase[0]
-        NClauses=TestCase[1]
-        LitsPerClause=TestCase[2]
-        Ntrials=TestCase[3]
-        #Now run the number of trials for this wff configuration
-        Scount=Ucount=0
-        AveStime=AveUtime=0
-        MaxStime=MaxUtime=0
-        for j in range(0,Ntrials):
-            #generate next trial case for this configuration
-            Nwffs=Nwffs+1
-            random.seed(ProbNum)
-            wff = build_wff(Nvars,NClauses,LitsPerClause)
-            results=test_wff(wff,Nvars,NClauses)
-            wff=results[0]
-            Assignment=results[1]
-            Exec_Time=results[3]
-            if results[2]:
-                y='S'
-                Scount=Scount+1
-                AveStime=AveStime+Exec_Time
-                MaxStime=max(MaxStime,Exec_Time)
-                Nsat=Nsat+1
-            else:
-                y='U'
-                Ucount=Ucount+1
-                AveUtime=AveUtime+Exec_Time
-                MaxUtime=max(MaxUtime,Exec_Time)
-                Nunsat=Nunsat+1
-            x=str(ProbNum)+','+str(Nvars)+','+str(NClauses)+','+str(LitsPerClause)
-            x=x+str(NClauses*LitsPerClause)+','+y+',1,'+str(Exec_Time)
-            if results[2]:
-                for k in range(1,Nvars+1):
-                    x=x+','+str(Assignment[k])
-            print(x)
-            f1.write(x+'\n')
-            f2.write(x+'\n')
-            #Add wff to cnf file
-            if not(ShowAnswer):
-                y='?'
-            x="c "+str(ProbNum)+" "+str(LitsPerClause)+" "+y+"\n"
-            f3.write(x)
-            x="p cnf "+str(Nvars)+" "+str(NClauses)+"\n"
-            f3.write(x)
-            for i in range(0,len(wff)):
-                clause=wff[i]
-                x=""
-                for j in range(0,len(clause)):
-                    x=x+str(clause[j])+","
-                x=x+"0\n"
-                f3.write(x)
-            #Increment problem number for next iteration
-            ProbNum=ProbNum+1
-        counts='# Satisfied = '+str(Scount)+'. # Unsatisfied = '+str(Ucount)
-        maxs='Max Sat Time = '+str(MaxStime)+'. Max Unsat Time = '+str(MaxUtime)
-        aves='Ave Sat Time = '+str(AveStime/Ntrials)+'. Ave UnSat Time = '+str(AveUtime/Ntrials)
-        print(counts)
-        print(maxs)
-        print(aves)
-        f2.write(counts+'\n')
-        f2.write(maxs+'\n')
-        f2.write(aves+'\n')
-    x=cnffile+",TheBoss,"+str(Nwffs)+","+str(Nsat)+","+str(Nunsat)+","+str(Nwffs)+","+str(Nwffs)+"\n"
-    f1.write(x)
-    f1.close()
-    f2.close()
-    f3.close()
-
-# Following generates several hundred test cases of 10 different wffs at each size
-# and from 4 to 22 variables, 10 to 240 clauses, and 2 to 10 literals per clause 
-TestCases=[
-     [4,10,2,10],
-    [8,16,2,10],
-    [12,24,2,10],
-    [16,32,2,10],
-    [20,40,2,10],
-    [24,48,2,10],
-    [28,56,2,10]]
-
-TC2=[
-    [4,10,2,10],
-    [8,16,2,10],
-    [12,24,2,10]]
-
-# Following generates a bunch of 2 literal wffs
-SAT2=[
-    [4,9,2,10],
-    [8,18,2,10],
-    [12,20,2,10],
-    [16,30,2,10],
-    [18,32,2,10],
-    [20,33,2,10],
-    [22,38,2,10],
-    [24,43,2,10],
-    [26,45,2,10],
-    [28,47,2,10]
-    ]
-
-trace=True
-ShowAnswer=True # If true, record evaluation result in header of each wff in cnffile
-ProbNum = 3
-resultsfile = r'unitresultsfile'
-tracefile = r'unittracefile'
-cnffile = r'unitcnffile'# Each of these list entries describes a series of random wffs to generate
-
-#run_cases(TC2,ProbNum,resultsfile,tracefile,cnffile)
-#run_cases(SAT2,ProbNum,resultsfile,tracefile,cnffile)
-run_cases(TestCases,ProbNum,resultsfile,tracefile,cnffile) # This takes a Looong Time!! 40  minutes
-'''
